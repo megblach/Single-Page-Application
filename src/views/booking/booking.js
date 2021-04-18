be12ae0a1b72b6da694f06fe31bb038e1b280b05
megblach <malgorzatablach89@gmail.com>
2021-04-18 20:10:07 +0200

    display shoppingCArt and items' names

--- src/views/booking/booking.js
+++ src/views/booking/booking.js
@@ -1,10 +1,38 @@
 import $ from 'jquery';
-const shoppingCart = [
-  { itemId: 7, quantity: 1 },
-  { itemId: 8, quantity: 2 },
+import axios from 'axios';
+
+let shoppingCart = [
+  { itemId: 2, quantity: 1 },
+  { itemId: 4, quantity: 2 },
 ];
 
 export const shoppingCartView = () => {
   //return "Helloworld";
-      return $('<table></table>').append(shoppingCart.map((item)=>$('<tr><td>'+item.itemId+'</td></tr>')))
+  const shoppingCartContent = (table,treatments) => {
+      const rows= shoppingCart.map((item)=>$(`<tr>
+      <td>${treatments.filter(treatment => treatment.id == item.itemId)[0].name}</td>
+      <td>${item.quantity}</td>
+      <td>0</td>
+      <td><button type="button" data-id="${item.itemId}">Usuń produkt</button>
+      
+    </tr>`))
+    table.empty().append(rows)
+    table.find('button').on('click',(event)=>{
+     // alert($(event.target).data("id"))
+      const itemId=$(event.target).data("id")
+      shoppingCart=shoppingCart.filter((item)=>item.itemId!=itemId)
+      shoppingCartContent(table,treatments)
+    })
+  };
+
+   const table= $('<table></table>')
+   axios.get(`http://localhost:3000/treatments`)
+   .then(response => response.data)
+   .then(treatments => {
+    shoppingCartContent(table, treatments)
+   })
+   
+  return table
+  
+
 };
